6b8080e9e0e79d229f6126fe3899f8818a112a20
ishwarprajapati29 <ishwarprajapati.527@gmail.com>
2026-07-22 00:37:47 +0530

    Added Python Day 4 programs

---
+++ python basic/day 4/operator.py
@@ -0,0 +1,24 @@
+#Arithmethic operators
+a = 10
+b = 3
+print(a+b)#addition
+print(a-b)#subraction
+print(a*b)#multiplication
+print(a/b)#division
+print(a%b)#modules
+print(a**b)#exponentiation
+
+#relational operators
+x = 10
+y = 20
+print(x>y)#greater than
+print(x<y)#less than
+print(x>=y)#greater than or equal to
+print(x<=y)#less than or equal to 
+print(x==y)#equal to
+print(x!=y)#not equal to
+
+#assignment operators
+p = 10
+p += 5 #p = p + 5, -5, *5, /5, %5, **5
+print(p)
