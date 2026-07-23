628e2a984e2a9d6cdaf84c1af79068dbaf94eb99
ishwarprajapati29 <ishwarprajapati.527@gmail.com>
2026-07-23 23:10:01 +0530

    Added Python Day 5 programs

---
+++ python basic/day 5/typeconversion.py
@@ -0,0 +1,16 @@
+
+"""we have two conversion type first is type conversion and  second is
+ type casting. type conversion work like a automaticalya and type casting 
+ work like a manualy"""
+#type conversion
+a = 2
+b = 4.25
+
+sum = a+b #2.0+4.25 => 6.25
+print(sum)
+
+#type casting
+k = int("2") 
+d= 4.25
+sum = k+d
+print(sum)
